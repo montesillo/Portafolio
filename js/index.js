6e71a51a8537bebfd7ac76132304219331bef50b
Hiram Montesillo <montesillo25@gmail.com>
2020-08-09 22:56:04 -0500

    Lincks de redes sociales

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function redirect(numero){
+    if(numero === 1){
+        location.href ="https://montesillo.github.io/Curso-de-jQuery-a-JavaScript/";
+    }else if(numero === 2){
+        location.href = "https://montesillo.github.io/clasesBoostrap/PlatziConf/";
+    }else if(numero === 3){
+        location.href = "https://montesillo.github.io/Curso-de-HTML-y-CSS/";
+    }
+
+}
