e30e68eaa6e6354ba4fdcb4f58271f5c5e7d8ec2
Hiram Montesillo <montesillo25@gmail.com>
2021-02-15 00:08:18 -0600

    Tipo de letra y acomodar la posición tanto del nav como elementos

--- src/componets/AboutMe.js
+++ src/componets/AboutMe.js
@@ -7,6 +7,9 @@ function AboutMe() {
         <section className="aboutMe">
             <h2>Hola, mi nombre es Hiram Montesillo</h2>
             <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEbzFeqPXe0_Q/profile-displayphoto-shrink_800_800/0/1581886068329?e=1619049600&v=beta&t=Q2aKI7V7vZAmUF8TksHZIqCz1hy1CXlkx-ViOb8IeXE" alt="Hiram Montesillo"/>
+            <div className="aboutMe__title">
+                <p >Frontend Developer</p>
+            </div>
             <p>Soy un apasionado por la tecnología, me encanta aprender y obtener nuevos retos.</p>
         </section>
     )
